503b98f07529bc41ec82c3926b9a53cfa5fe078c
aleshim <alex.chpak@gmail.com>
2022-05-01 11:21:57 +0800

    сделали item слайдера и анимацию

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-	// $('body').hide()
+	const showSlider = new Swiper('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1800,
+		centeredSlides: true
+	})
 
 	document.querySelector('video').playbackRate = 2;
 })
